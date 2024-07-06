2f8053e85d17eb84859bd75447c447fb8b8824fe
Qbuiba <37890918+Qbuiba@users.noreply.github.com>
2024-07-06 07:18:04 -0500

    update App.js with Dashboard component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return(
@@ -15,11 +16,15 @@ function App() {
           <li>
             <Link to="/login">Login</Link>
           </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
         </ul>
       </nav>
       <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Routes>
     </div>
   </Router>
